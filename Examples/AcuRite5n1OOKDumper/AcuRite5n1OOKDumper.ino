66cfc22f44e64934c6f58739ca7b2c8bf6488235
Chad Hinton <chadhinton@ChadsMac.thehintonfamily.us>
2018-08-05 18:45:31 -0500

    Various changes to integrate the Interrupt into the processing loop.  Spelling and syntax errors.

--- Examples/AcuRite5n1OOKDumper/AcuRite5n1OOKDumper.ino
+++ Examples/AcuRite5n1OOKDumper/AcuRite5n1OOKDumper.ino
@@ -1,3 +1,4 @@
+
 //
 // Simple OOK playground -- receives and dumps received signal and timing
 //
@@ -14,66 +15,46 @@
 #define TOL 50 // +- tolerance
 //
 // Lifted from acurite.c of rtl_433 project
-#define SHORT_LIMIT    = 220  // short pulse is 220 us + 392 us gap
-#define LONG_LIMIT     = 408  // long pulse is 408 us + 204 us gap
-#define SYNC_WIDTH     = 620  // sync pulse is 620 us + 596 us gap
-#define GAP_LIMIT      = 500  // longest data gap is 392 us, sync gap is 596 us
-#define RESET_LIMIT    = 4000 // packet gap is 2192 us
+#define SHORT_LIMIT  220  // short pulse is 220 us + 392 us gap
+#define LONG_LIMIT   408  // long pulse is 408 us + 204 us gap
+#define SYNC_WIDTH   620  // sync pulse is 620 us + 596 us gap
+#define GAP_LIMIT    500  // longest data gap is 392 us, sync gap is 596 us
+#define RESET_LIMIT  4000 // packet gap is 2192 us
 
 RFM69OOK radio;
 
-SimpleFIFO<uint32_t, 10> fifo;
+SimpleFIFO<uint8_t, 10> fifo;
 
-volatile uint32_t val, last_uS = 0;
+volatile uint8_t val = 0;
+volatile uint32_t last_uS = 0;
 volatile byte bits, lastPinState = 0;
 volatile bool gotone = false;
 volatile byte state = 0;
 
-unsigned long last_uS = 0; // 
-uint16_t tt[TSIZE];
-
-byte s0 = 0;
-byte pos = 0;
-bool restart = true;
-
 void setup() {
   Serial.begin(115200);
 
+  radio.attachUserInterrupt(interruptHandler);
   radio.initialize();
 //  radio.setBandwidth(OOK_BW_10_4);
 //  radio.setRSSIThreshold(-70);
   radio.setFixedThreshold(30);
 //  radio.setSensitivityBoost(SENSITIVITY_BOOST_HIGH);
-  radio.setFrequencyMHz(433.9);
+  radio.setFrequencyMHz(433.92);
   radio.receiveBegin();
+  val = 0;
+  last_uS = micros();  // prime it
 
   Serial.println(F("start"));
 }
 
 void loop() {
+  if (fifo.count() > 0) { // wait for a byte to accumulate
+    uint8_t lval = fifo.dequeue();
 
-  if (restart) {
-    while(!radio.poll());
-    last_uS = micros();
-    s0 = 1;
-    pos = 0;
-    restart = false;
-    return;
-  }
-
-  if (pos >= TSIZE || s0 == 0 && d > MAX_0_DUR) {
-    Serial.print("time: ");
-    Serial.println(micros() / 1000);
-    s = 1;
-    for (int i = 0; i < pos; i++) {
-      Serial.print(i);
-      Serial.print(F(": "));
-      Serial.print(s);
-      Serial.print(' ');
-      Serial.println(tt[i]);
-      s = !s;
-    }
-    restart = true;
+    Serial.print(micros() / 1000);
+    Serial.print(F(": "));
+    PrintHex83(&lval,4);
   }
 
 }
@@ -84,13 +65,13 @@ void interruptHandler(void) {
 
   bool currentPinState = radio.poll();            // get current state of pin
   uint32_t current_uS = micros();   // current uSecond counter
-  if (last_us < current_us) {  // Haven't wrapped around the micros() counter
+  if (last_uS < current_uS) {  // Haven't wrapped around the micros() counter
       uint32_t duration_uS = current_uS - last_uS;
 
         if (lastPinState != currentPinState) {  // this seems like an illogical check by definition
 
             if (currentPinState == 1) {  // end of LOW
-                if (duration_us > RESET_LIMIT) {  // We've waited too long, throw out the queue
+                if (duration_uS > RESET_LIMIT) {  // We've waited too long, throw out the queue
                     state = 0;
                     bits = 0;
                     val = 0;
@@ -101,12 +82,12 @@ void interruptHandler(void) {
                     bits = 0;
                 }
             } else {  // end of a HIGH
-                if (duration_us > RESET_LIMIT) {  // We've waited too long, throw out the queue
+                if (duration_uS > RESET_LIMIT) {  // We've waited too long, throw out the queue
                     state = 0;
                     bits = 0;
                     val = 0;
                     fifo.flush();
-                } else if (duration_us > SHORT_LIMIT) {  // This was a bit pulse
+                } else if (duration_uS > SHORT_LIMIT) {  // This was a bit pulse
                     val <<= 1;
                     bits++;
                     if (duration_uS > LONG_LIMIT) {  // This was a one pulse
@@ -123,4 +104,22 @@ void interruptHandler(void) {
 
 }
 
+void PrintHex83(uint8_t *data, uint8_t length) // prints 8-bit data in hex
+{
+    char tmp[length*2+1];
+    byte first ;
+    int j=0;
+    for (uint8_t i=0; i<length; i++)  {
+        first = (data[i] >> 4) | 48;
+        if (first > 57) tmp[j] = first + (byte)39;
+        else tmp[j] = first ;
+        j++;
 
+        first = (data[i] & 0x0F) | 48;
+        if (first > 57) tmp[j] = first + (byte)39; 
+        else tmp[j] = first;
+        j++;
+    }
+    tmp[length*2] = 0;
+    Serial.print(tmp);
+}
